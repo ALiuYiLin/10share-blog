77dac97e099736c32fbe6de99b936e160783446d
ALinYiLin <scliuyilin@163.com>
2025-09-20 12:12:40 +0800

    vite-vue-ssr project init

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { routes } from './router';
+const router = useRouter()
+</script>
 <template>
   <div>
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-    <router-view v-slot="{ Component }">
-      <Suspense>
-        <div>
-          <component :is="Component" />
-        </div>
-      </Suspense>
-    </router-view>
+    <div>
+      <ul>
+        <li v-for="route in routes" @click="router.push(route.path)">{{ route.name }}</li>
+      </ul>
+    </div>
+    <RouterView></RouterView>
   </div>
 </template>
-
-<style>
-@font-face {
-  font-family: 'Inter';
-  font-style: italic;
-  font-weight: 400;
-  font-display: swap;
-  src:
-    url('./assets/fonts/Inter-Italic.woff2#iefix') format('woff2'),
-    url('./assets/fonts/Inter-Italic.woff') format('woff');
-}
-.inter {
-  font-family: 'Inter';
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import vuePlugin from '@vitejs/plugin-vue'
+
+export default defineConfig(()=>({
+  resolve:{
+    alias:{
+      "@/*":"./src/*"
+    }
+  },
+  plugins:[
+    vuePlugin()
+  ]
+}))
